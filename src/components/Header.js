c8ad7192da02ea7c703b95fe9e466deafdbb69c1
felipeneneu <feliperpg3@gmail.com>
2024-04-22 14:04:29 -0300

    Banner and Header Criados

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+//logo
+import Logo from '../assets/logofn2.svg'
+
 const Header = () => {
   return (
-    <div className='bg-red-950'>
-      Header
-    </div>
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          {/*logo*/}
+          <a href="#">
+            <img src={Logo} alt='' className='lg:w-[300px] sm:w-[200px] w-[180px] h-auto' />
+          </a>
+          {/*button*/}
+          <button className='btn btn-sm'>Work with me</button>
+        </div>
+      </div>
+    </header>
   );
 };
 
