7e4e4282d5e2319c1e3b9c25c9bf0b2320c17475
felipeneneu <feliperpg3@gmail.com>
2024-04-23 01:24:32 -0300

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,16 +3,16 @@ import Logo from '../assets/logofn2.svg'
 
 const Contact = () => {
   return (
-    <footer className='min-h-[50vh] lg:min-h-[100vh] flex items-center' id='footer'>
+    <footer className='min-h-[50vh] lg:min-h-[30vh] flex items-center' id='footer'>
       <div className=' mx-auto'>
         <div className='flex flex-col items-center'>
           {/*logo*/}
           <a href="#">
-            <img src={Logo} alt='' className='lg:w-[600px] sm:w-[400px] w-[380px] h-auto' />
+            <img src={Logo} alt='' className='lg:w-[600px] sm:w-[400px] w-[380px] h-auto ' />
           </a>
           {/*button*/}
           <a target='_blank' href="https://w.app/vTWJ1z" >
-            <button className='mt-10 btn btn-sm items-center'>Entre em Contato</button></a>
+            <button className='mt-10 btn btn-sm items-center mb-40'>Entre em Contato</button></a>
         </div>
       </div>
     </footer>
